3effd57634f5d80820bdbacf5f17695c6295093c
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-05-10 16:09:52 +0545

    Making of cute about component

--- Notochan/src/pages/HomePage.jsx
+++ Notochan/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import FeaturePreview from "../components/FeaturePreview";
+import AboutSection from "../components/AboutSection";
 import Footer from "../components/Footer";
 
 const HomePage = () => {
@@ -11,6 +12,7 @@ const HomePage = () => {
       <Header />
       <HeroSection />
       <FeaturePreview />
+      <AboutSection/>
       <Footer/>
     </div>
   );
