3a12e432b60e962c88d8e35a9989dcbbb26c5b0d
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-05-10 19:08:51 +0545

    Making the footer

--- Notochan/src/components/Footer.jsx
+++ Notochan/src/components/Footer.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => {
-    return (
-        <footer className="bg-gray-100 text-center py-6 border-t">
-        <p className="text-[#4B0082] font-medium">
-          © {new Date().getFullYear()} NotoChan. All rights reserved.
-        </p>
-        <p className="text-sm text-[#4B0082]">Notes that make you smile ✨</p>
-      </footer>
-      
-    );
-  };
+  return (
+    <footer className='bg-gray-100 text-center py-6 border-t shadow-inner shadow-pink-100 mt-16 rounded-t-2xl'>
+      <p className='font-medium text-[#4B0082]'>
+         © {new Date().getFullYear()} NotoChan. All rights reserved.
+      </p>
+      <p className='text-sm text-[#4B0082] mt-1'>
+        Notes that make you smile ✨.
+      </p>
+    </footer>
+  )
+}
 
 export default Footer;
