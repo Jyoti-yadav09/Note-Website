2242729f02e3a2c0b94a50a396a0ce4653be17f2
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-05-12 15:17:27 +0545

    Adding the id in footer tag

--- Notochan/src/components/Footer.jsx
+++ Notochan/src/components/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-gray-100 text-center py-6 border-t shadow-inner shadow-pink-100 mt-16 rounded-t-2xl'>
+    <footer id="footer"
+     className='bg-gray-100 text-center py-6 border-t shadow-inner shadow-pink-100 mt-16 rounded-t-2xl'>
       <p className='font-medium text-[#4B0082]'>
          © {new Date().getFullYear()} NotoChan. All rights reserved.
       </p>
