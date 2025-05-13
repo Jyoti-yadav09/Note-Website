ee0569be6dc6cdc86f0ec0f227e8d2ad57eceeb7
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-05-13 18:35:16 +0545

    Installing react icons

--- Notochan/src/App.jsx
+++ Notochan/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
+import Dashboard from "./pages/Dashboard";
 import "./App.css";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/AuthPage" element={<AuthPage/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
     </Router>
   );
