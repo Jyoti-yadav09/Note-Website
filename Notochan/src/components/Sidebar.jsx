101fe83ba5749441d09c51f352d8a981f6e76116
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-05-14 20:36:01 +0545

    Adding icons on sidebar

--- Notochan/src/components/Sidebar.jsx
+++ Notochan/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-
+import {FaStickyNote, FaPlus, FaUser, FaClipboardList, FaArchive, FaTrashAlt, FaClock} from "react-icons/fa";
 
 const Sidebar = () => {
   return (
