d9a1d1a81c28bfa9d191ab96e4ea2e915b984255
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-05-28 14:53:29 +0545

    Add modals for viewing notes and setting reminders on MyNotes page

--- Notochan/src/pages/MyNote.jsx
+++ Notochan/src/pages/MyNote.jsx
@@ -91,7 +91,7 @@ const MyNotes = () => {
           </div>
         )}
 
-        {/* Note View Modal */}
+        {/* Note  Modal */}
         {selectedNote && (
           <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
             <div className="bg-white p-6 rounded shadow-md max-w-lg w-full relative">
